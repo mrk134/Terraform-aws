75d8f1f7520fbfe002b183c7206518722816a070
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-10 13:46:28 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,20 @@
 variable "public_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
-
-Example: ~/.ssh/terraform.pub
-DESCRIPTION
+type = "string"
 }
 
 variable "key_name" {
-  description = "Desired name of AWS key pair"
+  type = "string"
 }
 
 variable "aws_region" {
+  type = "string"
   description = "AWS region to launch servers."
   default     = "us-west-2"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_amis" {
+  type = "string"
   default = {
     eu-west-1 = "ami-674cbc1e"
     us-east-1 = "ami-1d4e7a66"
